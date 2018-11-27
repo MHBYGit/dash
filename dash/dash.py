2603b928ef82139a6387a7f9de316de8b13c06e0
Philippe Duval <philippe@plot.ly>
2018-11-27 16:26:01 -0500

    Add multi_output config value (Always true).

--- dash/dash.py
+++ dash/dash.py
@@ -333,6 +333,7 @@ class Dash(object):
 
     def _config(self):
         config = {
+            'multi_output': True,
             'url_base_pathname': self.url_base_pathname,
             'requests_pathname_prefix': self.config['requests_pathname_prefix']
         }
@@ -930,7 +931,9 @@ class Dash(object):
                 else:
                     response = {
                         'response': {
-                            output.component_property: output_value
+                            'props': {
+                                output.component_property: output_value
+                            }
                         }
                     }
 
